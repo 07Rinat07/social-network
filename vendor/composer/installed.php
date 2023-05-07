4f675a51d3e02ce7826802bb3aa4dc1743b32c3d
Rinat Sarmuldin <ura07srr@gmail.com>
2023-05-07 16:37:02 +0500

    Refresh poject & clear

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3e25e414e10dd585910dfd44f0b3e09cfe14257d',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -410,9 +410,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3e25e414e10dd585910dfd44f0b3e09cfe14257d',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
